b06b5cce3fed0d10c1c82d946dd84a8b0fd05948
Jordin Gutierrez <sjgutierrez2016@gmail.com>
2024-05-31 17:42:33 -0500

    Tarea suma

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+let num1,num2;
+let suma = 0;
+
+num1 = Number(prompt('ingrese numero 1'));
+num2 = Number(prompt('ingrese numero 2'));
+
+suma = num1 + num2
+
+alert(`La suma de ${num1} + ${num2} es ${suma}`);
